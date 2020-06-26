c02db9b7c08fdcaff761e459a52f3248c9048bbc
Bryce Mecum <petridish@gmail.com>
2020-06-25 18:04:19 -0800

    Update for June 25th

--- daily_forecast/daily_forecast.R
+++ daily_forecast/daily_forecast.R
@@ -7,7 +7,7 @@ library(readr)
 library(dplyr)
 
 # Load data
-inseason <- read_csv("data/big_eddy_set.csv") %>%
+inseason <- read_csv("data/bedrift.csv") %>%
   mutate(ccpue = cumsum(cpue))
 logistic_curve <- read_csv("pre-season-forecast/output/logistic_curve.csv")
 predictions <- read_csv("pre-season-forecast/output/predictions.csv")
